b8953941a6009845fe002839efe58577a17a9cec
prabhakar.bodipalli@capgemini.com <prabhakar.bodipalli@capgemini.com>
2017-02-01 03:01:38 +0530

    service added to twitter

---
+++ modules/custom/twitter/src/TwitterAPIExchange.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\twitter;
+
+
+/**
+ * Class TwitterAPIExchange.
+ *
+ * @package Drupal\twitter
+ */
+class TwitterAPIExchange implements TwitterAPIExchangeInterface {
+  /**
+   * Constructor.
+   */
+  public function __construct() {
+
+  }
+
+}
---
+++ modules/custom/twitter/src/TwitterAPIExchangeInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\twitter;
+
+/**
+ * Interface TwitterAPIExchangeInterface.
+ *
+ * @package Drupal\twitter
+ */
+interface TwitterAPIExchangeInterface {
+
+
+}
