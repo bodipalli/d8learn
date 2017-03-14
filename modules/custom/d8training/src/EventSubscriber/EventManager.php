558a5919153f447ea955414936adc3cbc34a27d9
prabhakarb <prabhakar.bodipalli@capgemini.com>
2017-03-14 14:38:56 +0530

    Code formats applied

--- modules/custom/d8training/src/EventSubscriber/EventManager.php
+++ modules/custom/d8training/src/EventSubscriber/EventManager.php
@@ -6,25 +6,20 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
-
 /**
 * 
 */
 class EventManager implements EventSubscriberInterface 
-{
-	
+{	
 	public static function getSubscribedEvents(){
-        $events[KernelEvents::RESPONSE][] = array('addEvents');
-       return $events;
-
+    $events[KernelEvents::RESPONSE][] = array('addEvents');
+    return $events;
+    
 	}
-
 	public function addEvents(FilterResponseEvent  $filterRsevent){
 
 		$response = $filterRsevent->getRespone();
-     	$response->headers->add(['Acces-Control-Allow-Origin']=>'*');
+    $response->headers->add(['Acces-Control-Allow-Origin'] => '*');
 
   }
-
-
 }
